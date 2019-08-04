e3855fdfa392790f8b95696fc35641794636abb9
Tate Shepard <tateshepard@yahoo.com>
2019-08-04 22:22:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,61 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {
+  "Theo" => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway"]
+  },
+  "Peter Jr." => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Library"]
+  },
+  "Lucky" => {
+    :color => ["purple"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  },
+  "Ms. K" => {
+    :color => ["grey", "white"],
+    :gender => ["female"],
+    :lives => ["Central Park"]
+  },
+  "Queenie" => {
+    :color => ["white", "brown"],
+    :gender => ["female"],
+    :lives => ["Subway"]
+  },
+  "Andrew" => {
+    :color => ["white"],
+    :gender => ["male"],
+    :lives => ["City Hall"]
+  },
+  "Alex" => {
+    :color => ["white", "brown"],
+    :gender => ["male"],
+    :lives => ["Central Park"]
+  }
+}
+  def nyc_pigeon_organizer(data)
+    
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  new_hash
+end
 end
